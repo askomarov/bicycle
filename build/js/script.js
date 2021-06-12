18d4d2d0ce7d1257f13e26f4763f388e29529203
askomarov <askomarov13@mail.ru>
2021-06-12 16:47:02 +0300

    начат новый проект

---
+++ build/js/script.js
@@ -0,0 +1,39 @@
+/* eslint-env es6 */
+'use strict';
+//  ////////////// меню бургер
+const headerMenu = document.querySelector('.header-menu');
+const menuButton = document.querySelector('.menu-button');
+
+
+const showMenu = () => {
+  document.body.classList.add('lock');
+  menuButton.setAttribute('aria-expanded', 'true');
+  menuButton.setAttribute('aria-label', 'закрыть меню');
+  headerMenu.classList.remove('header-menu--closed');
+  headerMenu.classList.add('header-menu--opened');
+};
+
+const closeMenu = () => {
+  document.body.classList.remove('lock');
+  menuButton.setAttribute('aria-expanded', 'false');
+  menuButton.setAttribute('aria-label', 'открыть меню');
+  headerMenu.classList.add('header-menu--closed');
+  headerMenu.classList.remove('header-menu--opened');
+};
+
+// //////////////// КОНЕЦ меню бургер
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  headerMenu.classList.remove('header-menu--no-js');
+  headerMenu.classList.add('header-menu--closed');
+
+  menuButton.addEventListener('click', () => {
+    if (headerMenu.classList.contains('header-menu--closed')) {
+      showMenu();
+    } else {
+      closeMenu();
+    }
+  });
+});
